e477c79d298200594f47b3cfe7baf05d7261ac72
antvh <ant.v.h@hotmail.com>
2021-05-05 17:20:40 -0500

    Primer commit

---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html',
+})
+export class ListadoComponent {
+  heroes:string[]=['Spiderman','Ironman','Hulk','Thor'];
+  heroeBorrado:String='';
+
+  BorrarHeroe():void{
+    this.heroeBorrado= this.heroes.shift() || '';
+  }
+}
